d64c24517baf5f0d581d9032f44412aad6a47e01
Preston Woodard <prestonwoodard@Prestons-MacBook-Pro.local>
2020-06-27 12:54:06 -0700

    add ex. to main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 use chrono::prelude::*;
+use std::num::ParseFloatError;
 
 fn main() {
+    // use helper funcs to get ra-dec strs into decimal degrees
+    let m1_ra = match to_decimal_degrees("05h 34m 31.94s", Coord::RA) {
+        Ok(ra) => ra,
+        Err(e) => panic!("Error occured when parsing right ascension : {:?}", e),
+    };
 
+    let m1_dec = match to_decimal_degrees("+22° 00′ 52.2″", Coord::DEC) {
+        Ok(dec) => dec,
+        Err(e) => panic!("An error occured when parsing declination: {:?}", e),
+    };
+
+    let m1 = AstroObject::new("M1: Crab Nebula (Supernova Remnant)", m1_ra, m1_dec);
+
+    let location = GeoCoords {
+        lat: 34.0522,
+        long: 118.2437,
+    };
+
+    println!("{:?}", m1.coords_as_alt_az(location));
 }
 
 struct GeoCoords {
@@ -18,22 +37,23 @@ enum Coord {
     RA,
     DEC,
 }
-fn to_decimal_degrees(input: &str, coord_type: Coord) -> f32 {
+fn to_decimal_degrees(input: &str, coord_type: Coord) -> Result<f32, ParseFloatError> {
     let tokens: Vec<_> = input
-        .split(&[' ', 'h', 'm', 's', '°', '′', '+', '-'][..])
+        .split(&[' ', 'h', 'm', 's', '°', '′', '+', '-', '″'][..])
         .filter(|ch| !ch.is_empty())
         .collect();
-    let hours_or_degrees: f32 = tokens[0].parse().unwrap();
-    let mins: f32 = tokens[1].parse().unwrap();
-    let secs: f32 = tokens[2].parse().unwrap();
+
+    let hours_or_degrees: f32 = tokens[0].parse()?;
+    let mins: f32 = tokens[1].parse()?;
+    let secs: f32 = tokens[2].parse()?;
 
     let in_degrees = hours_or_degrees + (mins / 60.0) + (secs / 3600.0);
 
     if coord_type == Coord::RA {
-        return in_degrees * 15.0;
+        return Ok(in_degrees * 15.0);
     }
 
-    in_degrees
+    Ok(in_degrees)
 }
 
 fn calculate_days_since_j2000() -> f32 {
@@ -42,6 +62,7 @@ fn calculate_days_since_j2000() -> f32 {
     let days_since = (now - j2000).num_seconds() as f32 / (24.0 * 3600.0);
     days_since
 }
+
 fn calculate_local_sidereal_time(days_j2000: f32, long: f32) -> f32 {
     let now = Utc::now();
     let fraction_of_hour = now.minute() as f32 / 60.0;
@@ -50,6 +71,7 @@ fn calculate_local_sidereal_time(days_j2000: f32, long: f32) -> f32 {
     let local_siderial_time = (100.46 + 0.985647 * days_j2000 + long + 15.0 * ut + 360.0) % 360.0;
     local_siderial_time
 }
+
 fn calculate_alt_az(ha: f32, dec: f32, location: GeoCoords) -> (f32, f32) {
     let prelim_alt = (dec.to_radians().sin() * location.lat.to_radians().sin())
         + (dec.to_radians().cos() * location.lat.to_radians().cos() * ha.to_radians().cos());
@@ -71,6 +93,13 @@ fn calculate_alt_az(ha: f32, dec: f32, location: GeoCoords) -> (f32, f32) {
 }
 
 impl<'a> AstroObject<'a> {
+    fn new(obj_name: &str, right_ascension: f32, declination: f32) -> AstroObject {
+        AstroObject {
+            _name: obj_name,
+            right_ascension: right_ascension,
+            declination: declination,
+        }
+    }
     fn coords_as_alt_az(&self, location_info: GeoCoords) -> (f32, f32) {
         let days_j2000 = calculate_days_since_j2000();
         let local_sidereal_time = calculate_local_sidereal_time(days_j2000, location_info.long);
